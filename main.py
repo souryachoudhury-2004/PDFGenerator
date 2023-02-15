2935a0e4e69b0f9800e00ce680ada88cf03c5e02
Sourya Choudhury <souryachoudhury24@gmail.com>
2023-02-15 15:26:24 +0530

    Completed Project.

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import pandas as pd
 # FPDF creates pdf object instances
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
-# 'P' stands for portrait
+#P stands for portrait
+pdf.set_auto_page_break(auto=False, margin=0)
 
 #pdf.add_page()
 
@@ -20,29 +21,42 @@ pdf.set_font(family="Times", style="B", size=12)
 # pdf.cell(w=0, h=12, txt="Hi there!", align="L", ln=1, border=1)
 
 df = pd.read_csv("topics.csv")
+page_count = 0
 
 for index, row in df.iterrows():
     pdf.add_page()
+    page_count += 1
     # each pandas dataframe row acts like a dictionary
     pdf.set_font("Times", size=26, style="B")
     # sets text colour to grey
     pdf.set_text_color(50, 50, 50)
     pdf.cell(w=0, h=12, txt=f"{row['Order']}) {row['Topic']}:", ln=1)
     pdf.line(10, 23, 200, 23)
+
+    pdf.ln(268)
+    for i in range(33, 278, 10):
+        pdf.line(10, i, 200, i)
+
+    pdf.set_font(family="Times", size=12, style="I")
+    pdf.cell(w=0, h=6, txt=f"Page: {page_count}")
+
     extra_pages = int(row["Pages"])-1
     if extra_pages > 0:
         for i in range(0, extra_pages):
             pdf.add_page()
+            page_count += 1
+            pdf.ln(275)
+            for j in range(13, 278, 10):
+                pdf.line(10, j, 200, j)
+            pdf.set_font(family="Times", size=12, style="I")
+            pdf.cell(w=0, h=6, txt=f"Page: {page_count}")
 
 
 
 
 
 
-
-
-
-pdf.output("testoutput.pdf")
+pdf.output("output.pdf")
 
 
 
